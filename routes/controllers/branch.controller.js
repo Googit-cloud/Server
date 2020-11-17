e50bab5ceac84432e7f64b8c34301947b955eb43
KE LI <like19911209@hotmail.com>
2020-11-18 04:53:42 +0900

    feat: create controllers handling note list based on privateMode status

--- routes/controllers/branch.controller.js
+++ routes/controllers/branch.controller.js
@@ -1,5 +1,6 @@
 const User = require('../../models/User');
 const Branch = require('../../models/Branch');
+const BranchSharingInfo = require('../../models/BranchSharingInfo');
 
 exports.createBranch = async (req, res, next) => {
   try {
@@ -41,15 +42,44 @@ exports.unShiftNoteToBranch = async (req, res, next) => {
 
 exports.getBranches = async (req, res, next) => {
   try {
-    console.log(req.params, req.body, 'regular');
+    console.log('all')
     const userId = req.params.user_id;
     const users = await User.findById(userId);
-    console.log(users, ' users');
-    const branchId = users.my_branches;
-    const branches = branchId.map((id) => {
 
+    // get shared branches info through branchSharingInfo
+    const branchSharingInfoIds = users.shared_branches_info;
+    const sharedBranchesIds = await Promise.all(
+      branchSharingInfoIds.map(async (sharingInfo) => {
+        const branchesSharingInfo = await BranchSharingInfo.findById(sharingInfo);
+        return branchesSharingInfo.branch_id;
+      })
+    );
+
+    const sharedBranches = await Promise.all(
+      sharedBranchesIds.map(async (branchId) => {
+        const sharedBranch = await Branch.findById(branchId);
+        return sharedBranch;
+      })
+    );
+
+    // get branches by my_branches
+    const myBranchesIds = users.my_branches;
+    const myBranches = await Promise.all(
+      myBranchesIds.map(async (branchId) => {
+        const myBranch = await Branch.findById(branchId)
+        return myBranch;
+      })
+    );
+
+    const accessibleBranchList = [...sharedBranches, ...myBranches]
+    // console.log(userId, 'the userId of the current user in regular')
+    // console.log(sharedBranches, 'shared branch list from the current user in regular')
+    // console.log(myBranches, 'the list of the branches created by the current user in regular');
+
+    res.status(200).json({
+      result: 'ok',
+      data: accessibleBranchList
     });
-    res.json('success');
   } catch (err) {
     next(err);
   }
@@ -57,8 +87,24 @@ exports.getBranches = async (req, res, next) => {
 
 exports.getPrivateBranches = async (req, res, next) => {
   try {
+    console.log('private')
     console.log(req.params, req.originalUrl, 'private');
-    res.json('success');
+
+    const userId = req.params.user_id;
+    const users = await User.findById(userId);
+
+    const myBranchesIds = users.my_branches;
+    const myBranches = await Promise.all(
+      myBranchesIds.map(async (branchId) => {
+        const myBranch = await Branch.findById(branchId)
+        return myBranch;
+      })
+    );
+
+    res.json({
+      result: 'ok',
+      data: myBranches
+    });
   } catch (err) {
     next(err);
   }
